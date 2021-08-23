da6250ee13e004e4463ee748873dc44d5dfc4a79
Kaye Par <kaye.cenizal@gmail.com>
2021-08-23 08:34:47 +0800

    feat: Add header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="container">
+                <h1 className="header__title">Indecision</h1>
+                <h1 className="header__subtitle">Let the computer decide for you</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
