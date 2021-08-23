562c7c542064e120d8e5ddb998ddf5e5102e970d
Kaye Par <kaye.cenizal@gmail.com>
2021-08-23 08:34:33 +0800

    feat: Add base app

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+const IndecisionApp = () => {
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+        </div>
+    );
+};
+
+export default IndecisionApp;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <IndecisionApp />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
