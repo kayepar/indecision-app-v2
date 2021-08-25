8e3f15404500570c5000aafeedc3443383214bad
Kaye Par <kaye.cenizal@gmail.com>
2021-08-25 20:56:19 +0800

    refactor: Integrate reducers and contexts

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Header from './Header';
+import ActionButton from './ActionButton';
+import OptionsContext from '../context/optionsContext';
+import optionsReducer from '../reducers/optionsReducer';
+import PickedOptionContext from '../context/pickedOptionContext';
+import pickedOptionReducer from '../reducers/pickedOptionReducer';
 
 const IndecisionApp = () => {
+    const defaultOptions = ['one', 'two', 'three'];
+    const [options, dispatchOptions] = useReducer(optionsReducer, defaultOptions);
+    const [pickedOption, pickedOptionDispatch] = useReducer(pickedOptionReducer, undefined);
+
     return (
-        <div>
+        <OptionsContext.Provider value={{ options, dispatchOptions }}>
             <div>
                 <Header />
             </div>
-        </div>
+            <PickedOptionContext.Provider value={{ pickedOption, pickedOptionDispatch }}>
+                <div className="container container--content">
+                    <div>
+                        <ActionButton />
+                    </div>
+                </div>
+            </PickedOptionContext.Provider>
+            {
+                // <OptionModal selectedOption={selectedOption} handleClearSelectedOption={handleClearSelectedOption} />
+            }
+        </OptionsContext.Provider>
     );
 };
 
