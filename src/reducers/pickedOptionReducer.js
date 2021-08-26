bedcf79ed554f1055a1f8685439878308c40feae
Kaye Par <kaye.cenizal@gmail.com>
2021-08-26 14:56:44 +0800

    feat: Add action to clear selected option

--- src/reducers/pickedOptionReducer.js
+++ src/reducers/pickedOptionReducer.js
@@ -2,6 +2,8 @@ const pickedOptionReducer = (state = undefined, action) => {
     switch (action.type) {
         case 'SET_PICKED_OPTION':
             return action.option;
+        case 'CLEAR_PICKED_OPTION':
+            return undefined;
         default:
             return state;
     }
