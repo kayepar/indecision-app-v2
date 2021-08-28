3f01c4e813c8ec34c91bfe1cb3c9deaff54eacd6
Kaye Par <kaye.cenizal@gmail.com>
2021-08-28 15:56:57 +0800

    refactor: Organize context providers
    Compose Provider from within the context file.
    Export helper function for easy access to context

--- src/context/optionsContext.js
+++ src/context/optionsContext.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import OptionsReducer from '../reducers/optionsReducer';
 
-const optionsContext = React.createContext();
+const OptionsContext = React.createContext();
 
-export default optionsContext;
+const OptionsProvider = ({ children }) => {
+    const defaultOptions = ['one', 'two', 'three'];
+
+    const [options, optionsDispatch] = useReducer(OptionsReducer, defaultOptions);
+    const value = { options, optionsDispatch };
+
+    return <OptionsContext.Provider value={value}>{children}</OptionsContext.Provider>;
+};
+
+const useOptions = () => {
+    const context = React.useContext(OptionsContext);
+
+    return context;
+};
+
+export { useOptions, OptionsProvider as default };
--- src/context/pickedOptionContext.js
+++ src/context/pickedOptionContext.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import PickedOptionReducer from '../reducers/pickedOptionReducer';
 
-const selectedOptionContext = React.createContext();
+const PickedOptionContext = React.createContext();
 
-export default selectedOptionContext;
+const PickedOptionProvider = ({ children }) => {
+    const [pickedOption, pickedOptionDispatch] = useReducer(PickedOptionReducer, undefined);
+    const value = { pickedOption, pickedOptionDispatch };
+
+    return <PickedOptionContext.Provider value={value}>{children}</PickedOptionContext.Provider>;
+};
+
+const usePickedOption = () => {
+    const context = React.useContext(PickedOptionContext);
+
+    return context;
+};
+
+export { usePickedOption, PickedOptionProvider as default };
