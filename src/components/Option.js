e1abbf6e4bf3cbd12bd04fac067e21d0d004ec57
Kaye Par <kaye.cenizal@gmail.com>
2021-08-31 14:38:34 +0800

    feat: Add delete option functionality

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useOptionsContext } from '../context/optionsContext';
 
 const Option = (props) => {
+    const { optionsDispatch } = useOptionsContext();
+
+    const handleDeleteOption = (option) => {
+        optionsDispatch({ type: 'DELETE_OPTION', option });
+    };
+
     return (
         <div className="option">
             <p className="option__text">
                 {props.index}. {props.text}
             </p>
-            <button className="button button--link" onClick={() => {}}>
+            <button className="button button--link" onClick={() => handleDeleteOption(props.text)}>
                 x
             </button>
         </div>
