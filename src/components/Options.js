df5a79f696126c82e6104d2c2a95ff79d9c47fc9
Kaye Par <kaye.cenizal@gmail.com>
2021-09-01 09:46:35 +0800

    refactor: Use localstorage custom hook

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,31 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
-import Option from './Option';
 import { useOptionsContext } from '../context/optionsContext';
+import useLocalStorage from '../hooks/useLocalStorage';
+import Option from './Option';
+import OptionsMenu from './OptionsMenu';
 
 const Options = () => {
     const { options, optionsDispatch } = useOptionsContext();
+    const [optionsFromStorage, saveOptionsToStorage] = useLocalStorage('options', []);
 
-    // todo: convert these to a custom hook? https://dev.to/sanderdebr/building-a-custom-react-localstorage-hook-2bja
     // load options
     useEffect(() => {
-        console.log('load Options');
-        const optionsFromStorage = JSON.parse(localStorage.getItem('options'));
-
+        console.log('load options from storage');
         if (optionsFromStorage) {
             optionsDispatch({ type: 'LOAD_OPTIONS', options: optionsFromStorage });
         }
-    }, [optionsDispatch]);
+    }, []);
 
     // save option to localstorage
     useEffect(() => {
-        console.log('Options - load Options - changed options value');
-        localStorage.setItem('options', JSON.stringify(options));
+        console.log('save options to storage');
+        saveOptionsToStorage(options);
     }, [options]);
 
     return (
         <div className="widget">
             <div className="widget-header">
                 <h3 className="widget-header__title">Your Options</h3>
+                <OptionsMenu options={options} />
             </div>
             <div className="widget-content">
                 {options.length === 0 && <p className="widget__message">Please add an option to get started</p>}
