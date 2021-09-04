11b25e9eb6eb96bb484b281e6c5f19dd8aaa1ddb
Kaye Par <kaye.cenizal@gmail.com>
2021-09-04 21:08:25 +0800

    chore: Change subtitle element to h2

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="header">
             <div className="container">
                 <h1 className="header__title">Indecision</h1>
-                <h1 className="header__subtitle">Let the computer decide for you</h1>
+                <h2 className="header__subtitle">Let the computer decide for you</h2>
             </div>
         </div>
     );
