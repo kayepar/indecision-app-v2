4956f08d03a6e50440f5fd452f66bdb89906713d
Kaye Par <kaye.cenizal@gmail.com>
2021-09-28 08:13:57 +0800

    chore: Code cleanup

--- src/index.js
+++ src/index.js
@@ -13,15 +13,6 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// import SaveToLocalStorage from './playground/SaveToLocaleStorage';
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <SaveToLocalStorage />
-//     </React.StrictMode>,
-//     document.getElementById('root')
-// );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
