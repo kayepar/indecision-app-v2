fda2f2b16f2dc34150201a896f93fa50b9ae2689
Kaye Par <kaye.cenizal@gmail.com>
2021-10-25 20:29:28 +0800

    fix: Add another div to serve as body
    This will occupy the whole width and will display the scrollbar when needed

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -2,7 +2,6 @@ import '../wdyr';
 
 import React, { useReducer, useEffect, useState, useRef } from 'react';
 import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import Pagination from '@mui/material/Pagination';
@@ -14,6 +13,10 @@ import ActionContainer from './ActionContainer';
 import optionsReducer from '../reducers/optionsReducer';
 import autoDeleteReducer from '../reducers/autoDeleteReducer';
 import useLocalStorage from '../hooks/useLocalStorage';
+
+// todo: add tests for paging and tally
+// todo: check on mobile
+// todo: trim size of pagination especially on mobile
 
 const IndecisionApp = () => {
     const [optionsFromStorage, saveOptionsToStorage] = useLocalStorage('options', []);
@@ -63,42 +66,48 @@ const IndecisionApp = () => {
     return (
         <>
             <Header />
-            <div className="container container--content">
-                <ActionContainer
-                    options={options}
-                    optionsDispatch={optionsDispatch}
-                    autoDelete={autoDelete}
-                ></ActionContainer>
-                <AddForm options={options} optionsDispatch={optionsDispatch} />
+            <div className="body">
+                <div className="container container--content">
+                    <ActionContainer
+                        options={options}
+                        optionsDispatch={optionsDispatch}
+                        autoDelete={autoDelete}
+                    ></ActionContainer>
+                    <AddForm options={options} optionsDispatch={optionsDispatch} />
 
-                <OptionsHeader
-                    hasOptions={options.length > 0}
-                    optionsDispatch={optionsDispatch}
-                    autoDelete={autoDelete}
-                    autoDeleteDispatch={autoDeleteDispatch}
-                />
-                <Options options={optionsToDisplay} optionsDispatch={optionsDispatch} />
-                <div className="options-footer">
-                    <div className="tally">
-                        {options.length} {options.length !== 1 ? 'options' : 'option'}
-                    </div>
-                    <div className="pagination-container">
-                        <FormControl>
-                            <Select id="demo-simple-select-helper" value={displayPerPage} onChange={handleRowsOnChange}>
-                                <MenuItem value={5}>5</MenuItem>
-                                <MenuItem value={10}>10</MenuItem>
-                                <MenuItem value={20}>20</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <Pagination
-                            count={numPages}
-                            onChange={handlePageOnChange}
-                            page={page}
-                            variant="outlined"
-                            shape="rounded"
-                            size="medium"
-                            className="pagination"
-                        />
+                    <OptionsHeader
+                        hasOptions={options.length > 0}
+                        optionsDispatch={optionsDispatch}
+                        autoDelete={autoDelete}
+                        autoDeleteDispatch={autoDeleteDispatch}
+                    />
+                    <Options options={optionsToDisplay} optionsDispatch={optionsDispatch} />
+                    <div className="options-footer">
+                        <div className="tally">
+                            {options.length} {options.length !== 1 ? 'options' : 'option'}
+                        </div>
+                        <div className="pagination-container">
+                            <FormControl>
+                                <Select
+                                    id="demo-simple-select-helper"
+                                    value={displayPerPage}
+                                    onChange={handleRowsOnChange}
+                                >
+                                    <MenuItem value={5}>5</MenuItem>
+                                    <MenuItem value={10}>10</MenuItem>
+                                    <MenuItem value={20}>20</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <Pagination
+                                count={numPages}
+                                onChange={handlePageOnChange}
+                                page={page}
+                                variant="outlined"
+                                shape="rounded"
+                                size="medium"
+                                className="pagination"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
