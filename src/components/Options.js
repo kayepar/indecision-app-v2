07185f21b023d64129d55d2ce48e2ff14d504ec1
Kaye Par <kaye.cenizal@gmail.com>
2021-10-28 15:59:46 +0800

    refactor: Remove autoscroll feature
    Not needed anymore since the options list is now paginated

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,14 +1,11 @@
 import React, { memo } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import useAutoScroll from '../hooks/useAutoScroll';
 import Option from './Option';
 
 const Options = ({ options, optionsDispatch }) => {
-    const optionsContainerRef = useAutoScroll(options.length);
-
     return (
         <div className="widget">
-            <div className="widget-content" ref={optionsContainerRef} data-testid="options-container">
+            <div className="widget-content" data-testid="options-container">
                 {options.length === 0 && <p className="widget__message">Please add an option to get started</p>}
                 {options.map((option) => (
                     <Option optionsDispatch={optionsDispatch} data-testid="option-item" key={uuidv4()} text={option} />
