cac0679200684ca8727b059e2bd52483eecc1511
Kaye Par <kaye.cenizal@gmail.com>
2021-10-31 21:36:31 +0800

    refactor: Add default state value

--- src/reducers/paginationReducer.js
+++ src/reducers/paginationReducer.js
@@ -1,12 +1,21 @@
-const paginationReducer = (state, action) => {
+const defaultState = {
+    page: 0,
+    rowsPerPage: 5,
+};
+
+const paginationReducer = (state = defaultState, action) => {
+    // console.log(state);
+
     switch (action.type) {
         case 'SET_PAGE':
+            console.log(state);
             return { ...state, page: action.page };
 
         case 'SET_ROWS_PER_PAGE':
-            return { ...state, rowsPerPage: action.rows };
+            return { ...state, rowsPerPage: action.rowsPerPage };
         default:
-            throw new Error('Cannot handle action');
+            // throw new Error('Cannot handle action');
+            return defaultState;
     }
 };
 
